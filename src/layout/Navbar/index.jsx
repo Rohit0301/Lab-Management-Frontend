3a845a87f3a330f99eadff67462340250734292b
Crelio Health <creliohealth@Crelios-MacBook-Air.local>
2023-06-16 14:30:34 +0530

    minor fixes

--- src/layout/Navbar/index.jsx
+++ src/layout/Navbar/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Box, Button, Typography, useTheme } from "@mui/material";
+import { Avatar, Box, Button, Typography, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./index.module.css";
 import { logout } from "../../store/authSlice/action";
 
 export default function Navbar() {
 	const theme = useTheme();
-	const user = useSelector((state) => state.auth);
+	const auth = useSelector((state) => state.auth);
 	const dispatch = useDispatch();
-
+	const getUserName = () => {
+		if (auth.role === "user") return auth.user.full_name;
+		else return auth.user.name;
+	};
 	return (
 		<Box
 			bgcolor={theme.palette.primary.main}
@@ -23,14 +26,27 @@ export default function Navbar() {
 			<Typography variant="h5" color="white">
 				Live Health
 			</Typography>
-			{user.isLoggedIn && (
-				<Button
-					className={styles.auth_btn}
-					variant="outlined"
-					onClick={() => dispatch(logout())}
-				>
-					Logout
-				</Button>
+			{auth.isLoggedIn && (
+				<Box className={styles.container}>
+					<Box className={styles.container} style={{ gap: "8px" }}>
+						{getUserName() && (
+							<Avatar style={{ background: "#e36907" }}>
+								{getUserName().charAt(0)}
+							</Avatar>
+						)}
+						<Typography variant="body1" style={{ color: "white" }}>
+							{getUserName()}
+						</Typography>
+					</Box>
+
+					<Button
+						className={styles.auth_btn}
+						variant="outlined"
+						onClick={() => dispatch(logout())}
+					>
+						Logout
+					</Button>
+				</Box>
 			)}
 		</Box>
 	);
